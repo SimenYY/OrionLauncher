077a4f4ebd369880ebacd8d345a140f7a4f53764
AdzLovelace <heyinyu.sensi@foxmail.com>
2025-07-18 10:36:03 +0800

    refactor: 移除未使用的导入
    - 删除了 Utils 模块中的 app_lock 导入
    - 保留了其他现有的导入

--- orion.py
+++ orion.py
@@ -11,7 +11,6 @@ import sys
 
 from PySide6.QtCore import QLocale, QTranslator
 from PySide6.QtWidgets import QApplication
-from Utils import app_lock
 from View import MainWindow
 
 import init     # 导入 init.py 以初始化环境
